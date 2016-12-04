743c04c154f39f310b1a9578865b6f6ef2c9780a
Arakawa Shintaro <arashinbe@gmail.com>
2016-12-04 11:56:25 +0900

    update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161203113734) do
+ActiveRecord::Schema.define(version: 20161204025429) do
+
+  create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "text",       limit: 65535
     t.string   "image"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.integer  "group_id"
+    t.index ["group_id"], name: "index_messages_on_group_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
