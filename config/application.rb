f85185f0771d80d6cf569296e3e2fbee255888d2
Arakawa Shintaro <arashinbe@gmail.com>
2016-12-04 11:29:35 +0900

    delete unnecessary space

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
-
     config.generators do |g|
       g.helper         false
       g.stylesheets    false
