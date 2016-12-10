53a34bfd95013f2532bb2573de6b7cc6e036bbb6
Arakawa Shintaro <arashinbe@gmail.com>
2016-12-10 16:04:55 +0900

    define new and create#Groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,21 @@
 class GroupsController < ApplicationController
 
   def new
+    @group = Group.new
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to messages_path, notice: "チャットグループが作成されました。"
+    else
+      redirect_to new_group_path, alert: "チャットグループを作成できませんでした。"
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, user_ids: [])
   end
 end
